f7fb04802ebbe6941f4d8bae1c106bb69c23dd48
WilliamCadenas21 <williamcadenas21@gmail.com>
2023-07-02 11:28:41 -0500

    feat: added improved version of colro square

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,30 +2,48 @@ import { StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import ColorCounter from "../components/ColorCounter";
 
-const INCREMENT = 50
+const INCREMENT = 25;
 
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
+  const setColor = (color, change) => {
+    //color === 'red', 'green', 'blue'
+    //change === +15, -15
+
+    switch (color) {
+      case "red":
+        red + change > 255 || red + change < 0 || setRed(red + change);
+        break;
+      case "blue":
+        blue + change > 255 || blue + change < 0 || setBlue(blue + change);
+        break;
+      case "green":
+        green + change > 255 || green + change < 0 || setGreen(green + change);
+        break;
+      default:
+        break;
+    }
+  };
   return (
     <View>
       <Text>SquareScreen</Text>
       <ColorCounter
         color="Red"
-        onIncrease={() => setRed(red + INCREMENT)}
-        onDecrease={() => setRed(red - INCREMENT)}
+        onIncrease={() => setColor("red", INCREMENT)}
+        onDecrease={() => setColor("red", -INCREMENT)}
       />
       <ColorCounter
         color="Blue"
-        onIncrease={() => setBlue(blue + INCREMENT)}
-        onDecrease={() => setBlue(blue - INCREMENT)}
+        onDecrease={() => setColor("blue", - INCREMENT)}
+        onIncrease={() => setColor("blue", + INCREMENT)}
       />
       <ColorCounter
         color="Green"
-        onIncrease={() => setGreen(green + INCREMENT)}
-        onDecrease={() => setGreen(green - INCREMENT)}
+        onIncrease={() => setColor("green", + INCREMENT)}
+        onDecrease={() => setColor("green", - INCREMENT)}
       />
       <View
         style={{
