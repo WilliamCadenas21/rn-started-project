643eac6100f104981e266a23dc917a4aa651c54c
WilliamCadenas21 <williamcadenas21@gmail.com>
2023-07-02 10:53:31 -0500

    feat: adding a cutom color square

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,12 +1,13 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const ColorCounter = ({ color }) => {
+const ColorCounter = ({ color, onIncrease, onDecrease }) => {
+
   return (
     <View>
       <Text>{color}</Text>
-      <Button title={`increase ${color}`} />
-      <Button title={`decrease ${color}`} />
+      <Button onPress={() => onIncrease()} title={`Increase ${color}`} />
+      <Button onPress={() => onDecrease()} title={`Decrease ${color}`} />
     </View>
   );
 };
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,14 +1,39 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import ColorCounter from "../components/ColorCounter";
 
+const INCREMENT = 50
+
 const SquareScreen = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
   return (
     <View>
       <Text>SquareScreen</Text>
-      <ColorCounter color="Red"/>
-      <ColorCounter color="Blue"/>
-      <ColorCounter color="Green"/>
+      <ColorCounter
+        color="Red"
+        onIncrease={() => setRed(red + INCREMENT)}
+        onDecrease={() => setRed(red - INCREMENT)}
+      />
+      <ColorCounter
+        color="Blue"
+        onIncrease={() => setBlue(blue + INCREMENT)}
+        onDecrease={() => setBlue(blue - INCREMENT)}
+      />
+      <ColorCounter
+        color="Green"
+        onIncrease={() => setGreen(green + INCREMENT)}
+        onDecrease={() => setGreen(green - INCREMENT)}
+      />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red},${green},${blue})`,
+        }}
+      />
     </View>
   );
 };
@@ -16,4 +41,3 @@ const SquareScreen = () => {
 const styles = StyleSheet.create({});
 
 export default SquareScreen;
-
