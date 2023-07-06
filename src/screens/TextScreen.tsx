cae2ecf07dc43ad11dd816a57cb17c893b6d37d4
WilliamCadenas21 <williamcadenas21@gmail.com>
2023-07-05 21:58:46 -0500

    feat: handle a input

--- src/screens/TextScreen.tsx
+++ src/screens/TextScreen.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput } from "react-native";
 
 const TextScreen = () => {
+  const [password, setPassword] = useState("");
   return (
     <View>
-      <Text>TextScreen</Text>
+      <Text style={styles.title}>TextScreen</Text>
       <TextInput
         style={styles.input}
+        value={password}
         autoCapitalize="none"
         autoCorrect={false}
+        onChangeText={(newValue) => {
+          setPassword(newValue);
+        }}
       />
+      <Text>here is the input: {password}</Text>
+      {password.length < 5  ? <Text>password must be longer than 5 characters</Text>: null}
     </View>
   );
 };
@@ -19,6 +26,9 @@ const styles = StyleSheet.create({
     margin: 15,
     borderColor: "black",
     borderWidth: 1,
+  },
+  title: {
+    fontSize: 25,
   },
 });
 
